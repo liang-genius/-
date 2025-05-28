35f094d0201365dc52ddbb9d850bc8b1c044fa2b
liang-genius <58184083+liang-genius@users.noreply.github.com>
2025-05-28 18:25:18 +0800

    Create bubbleSort.java

---
+++ Sort/bubbleSort.java
@@ -0,0 +1,23 @@
+/**
+ * 冒泡排序
+ * @param arr
+ * @return arr
+ */
+
+public static int[] bubbleSort(int[] arr){
+  for(int i = 1; i < arr.length(); i++){
+    boolean flag = true;
+    for(int j = 0; j < arr.length() - i; j++){
+      if(arr[j] > arr[j + 1]){
+        int tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+        flag = false;
+      }
+    }
+    if(flag)
+      break;
+  }
+  return arr;
+}
+     
