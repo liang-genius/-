b3364fb59e1480a912ecce680847eb0d953dcf6c
liang-genius <58184083+liang-genius@users.noreply.github.com>
2025-05-28 18:42:16 +0800

    Update selectionSort.java

--- Sort/selectionSort.java
+++ Sort/selectionSort.java
@@ -6,4 +6,15 @@
 public static int[] selectionSort(int[] arr){
   for(int i = 0; i < arr.length; i++){
     int minIndex = i;
-    for(int j = i + 1; j < arr.length;
+    for(int j = i + 1; j < arr.length; j++){
+      if(arr[j] < arr[minIndex])
+        minIndex = j;
+    }
+    if(minIndex != i){
+      int tmp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = tmp;
+    }
+  }
+  return arr;
+}
